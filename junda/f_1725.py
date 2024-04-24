f44544d568076ad4141864acc582af3ca8f78b5f
James Hoang <hvdthong@gmail.com>
2024-04-24 11:38:23 +1000

    f_1725: remove import numpy as np, import warning to remove warnings, change TestF1725 to TestCases

--- junda/f_1725.py
+++ junda/f_1725.py
@@ -1,7 +1,7 @@
-import numpy as np
 from scipy.stats import linregress
 import matplotlib.pyplot as plt
 import pandas as pd
+import warnings
 
 def f_1725(data, column1, column2):
     """
@@ -21,10 +21,10 @@ def f_1725(data, column1, column2):
     ValueError: If the specified columns do not exist in the DataFrame.
 
     Requirements:
-    - numpy
     - scipy.stats
     - matplotlib.pyplot
     - pandas
+    - warnings
 
     Example:
     >>> data = pd.DataFrame([[14, 25], [1, 22], [7, 8]], columns=['Column1', 'Column2'])
@@ -36,7 +36,9 @@ def f_1725(data, column1, column2):
     x = data[column1].values
     y = data[column2].values
 
-    slope, intercept, r_value, p_value, std_err = linregress(x, y)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", category=RuntimeWarning)
+        slope, intercept, r_value, p_value, std_err = linregress(x, y)
 
     fig, ax = plt.subplots()
     ax.plot(x, y, 'o', label='original data')
@@ -47,7 +49,7 @@ def f_1725(data, column1, column2):
 
 import unittest
 
-class TestF1725(unittest.TestCase):
+class TestCases(unittest.TestCase):
     def setUp(self):
         self.data = pd.DataFrame({
             'Column1': [14, 1, 7, 10, 5],
@@ -74,18 +76,26 @@ class TestF1725(unittest.TestCase):
     def test_single_point_regression(self):
         single_point_data = pd.DataFrame({'Column1': [1], 'Column2': [2]})
         result, ax = f_1725(single_point_data, 'Column1', 'Column2')
-        import math
         # self.assertEqual(result[0], np.nan)
         self.assertEqual(result[2], 0)  # Slope should be 0 for single point
     
     def test_return_values(self):
         result, ax = f_1725(self.data, 'Column1', 'Column2')
-        print(result)
+        # print(result)
         # with open('df_contents.txt', 'w') as file:
         #     file.write(str(result))
         expect = (0.3456790123456789, 13.641975308641975, 0.23699046752221187, 0.7011032163730078, 0.8181438416490141)
         for res, exp in zip(result, expect):
             self.assertAlmostEqual(res, exp, places=7) 
 
-if __name__ == '__main__':
-    unittest.main()
+def run_tests():
+    """Run all tests for this function."""
+    loader = unittest.TestLoader()
+    suite = loader.loadTestsFromTestCase(TestCases)
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    run_tests()
