d6227242568683a2e4f43ae8895f69dceeb8cd40
James Hoang <hvdthong@gmail.com>
2024-04-24 12:38:12 +1000

    f_1727: import pandas and numpy below unittest, change TestF1727 to TestCases

--- junda/f_1727.py
+++ junda/f_1727.py
@@ -42,8 +42,10 @@ def f_1727(data, n_components=2):
     return pd.DataFrame(transformed_data, columns=[f'PC{i+1}' for i in range(n_components)]), ax
 
 import unittest
+import pandas as pd
+import numpy as np
 
-class TestF1727(unittest.TestCase):
+class TestCases(unittest.TestCase):
     def setUp(self):
         self.data = pd.DataFrame({
             'Column1': np.random.rand(10),
@@ -76,5 +78,15 @@ class TestF1727(unittest.TestCase):
         expect = ['-0.36270132751314693,-0.17330242962071069', '0.7073025303719391,0.12382897836601565', '0.45378164000836924,0.1734575007991456', '-0.06806713223200053,-0.18707071063291186', '-0.41150042971259093,0.09384691859758798', '-0.4104362188060755,0.09501439103733277', '-0.3990216926714853,0.2501208456858351', '0.34082913981297874,-0.14263963596803247', '0.08412503285413396,-0.028734567486117184', '0.06568845788787812,-0.20452129077814485']
 
         self.assertEqual(df_list, expect, "DataFrame contents should match the expected output")
-if __name__ == '__main__':
-    unittest.main()
+
+def run_tests():
+    """Run all tests for this function."""
+    loader = unittest.TestLoader()
+    suite = loader.loadTestsFromTestCase(TestCases)
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    run_tests()
