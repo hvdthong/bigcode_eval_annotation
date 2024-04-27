c57a1f7e88224aaa71107039705d98560606db6b
James Hoang <hvdthong@gmail.com>
2024-04-27 20:32:37 +1000

    f_1727: import pandas and numpy below unittest, change TestF1727 to TestCases, modify the test_values function

--- junda/f_1727.py
+++ junda/f_1727.py
@@ -75,9 +75,16 @@ class TestCases(unittest.TestCase):
         # with open('df_contents.txt', 'w') as file:
         #     file.write(str(df_list))
 
+        # Convert string pairs to list of tuples of floats
         expect = ['-0.36270132751314693,-0.17330242962071069', '0.7073025303719391,0.12382897836601565', '0.45378164000836924,0.1734575007991456', '-0.06806713223200053,-0.18707071063291186', '-0.41150042971259093,0.09384691859758798', '-0.4104362188060755,0.09501439103733277', '-0.3990216926714853,0.2501208456858351', '0.34082913981297874,-0.14263963596803247', '0.08412503285413396,-0.028734567486117184', '0.06568845788787812,-0.20452129077814485']
+        # self.assertEqual(df_list, expect, "DataFrame contents should match the expected output")
+        df_tuples = [tuple(map(float, item.split(','))) for item in df_list]
+        expect_tuples = [tuple(map(float, item.split(','))) for item in expect]
 
-        self.assertEqual(df_list, expect, "DataFrame contents should match the expected output")
+        # Assert each pair of tuples is approximately equal
+        for actual, expected in zip(df_tuples, expect_tuples):
+            self.assertAlmostEqual(actual[0], expected[0], places=7, msg="DataFrame contents should match the expected output")
+            self.assertAlmostEqual(actual[1], expected[1], places=7, msg="DataFrame contents should match the expected output")
 
 def run_tests():
     """Run all tests for this function."""
