1347ace92304e3dfd69a056cc062ea11d7c8f38e
James Hoang <hvdthong@gmail.com>
2024-04-27 21:10:31 +1000

    f_1739: change TestF1739 to TestCases

--- junda/f_1739.py
+++ junda/f_1739.py
@@ -57,7 +57,7 @@ def f_1739(df):
 import unittest
 
 
-class TestF1739(unittest.TestCase):
+class TestCases(unittest.TestCase):
 
     def setUp(self):
         self.valid_df = pd.DataFrame({
@@ -105,5 +105,14 @@ class TestF1739(unittest.TestCase):
         np.testing.assert_array_almost_equal(heatmap_data_reshaped, expected_corr_matrix)
 
 
-if __name__ == '__main__':
-    unittest.main()
+def run_tests():
+    """Run all tests for this function."""
+    loader = unittest.TestLoader()
+    suite = loader.loadTestsFromTestCase(TestCases)
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    run_tests()
